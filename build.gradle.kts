ad0953464eebdb23efa5b288138d7526c150e5af
shadshuvo <shad@freedrivemovie.com>
2024-10-02 06:52:02 +0600

    Add necessary dependencies and update GitHub Actions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,17 @@ plugins {
 
 repositories {
     mavenCentral()
+    google() // For Android dependencies
+    jcenter() // For additional dependencies
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("com.github.cloudstream3:cloudstream3:1.0.0") // Replace with the correct version
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.json:json:20210307")
+    implementation("org.jsoup:jsoup:1.13.1")
+    implementation("androidx.core:core-ktx:1.6.0")
     testImplementation(kotlin("test"))
 }
 
